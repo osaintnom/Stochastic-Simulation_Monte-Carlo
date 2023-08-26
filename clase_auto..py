484aa674e0bd81a59d5458e6aac5f85409e6a4fe
fede-pereira <124898426+fede-pereira@users.noreply.github.com>
2023-08-26 13:59:03 -0300

    Genesis
    agrego los tres scripts principales

---
+++ clase_auto..py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Auto:
+    def __init__(self, x, y, velocidad, color, nombre):
+        self.x = x
+        self.y = y
+        self.velocidad = velocidad
+        self.color = color
+        self.nombre = nombre
+
+    def avanzar(self):
+        self.x += self.velocidad
+
+    def retroceder(self):
+        self.x -= self.velocidad
+
+    def __str__(self):
+        return self.nombre
---
+++ clase_ruta.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+
+from clase_auto import Auto
