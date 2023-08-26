123ec4735a91c7a7ce97de20e259138a1fe45a18
fede-pereira <124898426+fede-pereira@users.noreply.github.com>
2023-08-26 14:10:09 -0300

    Update clase_ruta.py

--- clase_ruta.py
+++ clase_ruta.py
@@ -4,3 +4,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from clase_auto import Auto
+
+class Ruta:
+    def __init__(self, autos):
+        self.autos = autos
+        self.fig, self.ax = plt.subplots()
+        self.xdata, self.ydata = [], []
+        self.ln, = plt.plot([], [], 'ro', animated=True)
+        self.ax.set_xlim(0, 100)
+        self.ax.set_ylim(0, 100)
+        self.ax.grid()
+        self.ax.set_title('Ruta de autos')
+        self.ax.set_xlabel('Posición en x')
+        self.ax.set_ylabel('Posición en y')
+
+    def init(self):
+        self.ln.set_data([], [])
+        return self.ln,
+
+    def update(self, frame):
+        self.xdata, self.ydata = [], []
+        for auto in self.autos:
+            auto.avanzar()
+            self.xdata.append(auto.x)
+            self.ydata.append(auto.y)
+        self.ln.set_data(self.xdata, self.ydata)
+        return self.ln,
+
+    def animar(self):
+        ani = animation.FuncAnimation(self.fig, self.update, frames=np.linspace(0, 100, 100), init_func=self.init, blit=True, interval=100, repeat=False)
+        plt.show()
