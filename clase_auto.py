2e3cbcf3bb43a701c880e4b4f01769ed6865005e
Olivier Saint Nom <oltie4002@gmail.com>
2023-09-12 20:19:42 -0300

    docstrings clase auto

--- clase_auto.py
+++ clase_auto.py
@@ -6,66 +6,65 @@ import random
 import threading as th
 import time
 
-
+# Define a class for simulating cars
 class Auto:
-    def __init__(self, x, y, velocidad, color, nombre, x_max=1000, y_max=10,next_car=None,mean = 2.2, delta = 1, alpha = 0.05):
-        self.x = x
-        self.y = y # por ahora no hace nada pero seria como cambiar de carril
-        self.x_max = x_max  # marca donde termina la ruta
-        self.velocidad = velocidad
-        self.color = color # abria que aprender a usar esto para que se vea mas lindo
-        self.nombre = nombre # inutil pero qsy despues puede servir
-        self.next_car = next_car 
-        self.mean = mean
-        self.delta = delta
-        self.alpha = alpha
-        self.reaction_time_mean = random.normalvariate(0.08,0.01)
-        self.aceleracion_max = random.lognormvariate(0.3,0.05)
+    def __init__(self, x, y, velocidad, color, nombre, x_max=1000, y_max=10, next_car=None, mean=2.2, delta=1, alpha=0.05):
+        # Initialize car properties
+        self.x = x               # Current x-coordinate of the car
+        self.y = y               # Current y-coordinate of the car
+        self.x_max = x_max       # Maximum x-coordinate the car can reach
+        self.velocidad = velocidad  # Current velocity of the car
+        self.color = color       # Color of the car
+        self.nombre = nombre     # Name or identifier of the car
+        self.next_car = next_car # Reference to the next car in the lane
+        self.mean = mean         # Mean velocity (used for acceleration)
+        self.delta = delta       # Not used in the code
+        self.alpha = alpha       # Not used in the code
+        self.reaction_time_mean = random.lognormvariate(0.08,0.01)  # Reaction time of the driver
+        self.aceleracion_max = random.lognormvariate(0.3, 0.05)  # Maximum acceleration of the car
+        
+        # Ensure acceleration and braking limits are within bounds
         if self.aceleracion_max > 0.42:
             self.aceleracion_max = 0.42
-
-        self.frenado_max = random.lognormvariate(0.7,0.1)
-        # self.frenado_max = 1
+        self.frenado_max = random.lognormvariate(0.7, 0.1)  # Maximum deceleration (braking) of the car
         if self.frenado_max > 0.8:
             self.frenado_max = 0.8
-        self.quieto = False
-        self.quieto_count = 0
-        self.colision = False
-        self.tiempo_inicio = time.time()
-        self.tiempo_terminar = 0
-        self.historic_velocidad = []
-        # agregar velocidad maxima?
-        self.vel_max = 2.2
-        # Genera un hilo para acelerar el auto
+        
+        self.quieto = False       # Flag indicating if the car is stationary
+        self.quieto_count = 0     # Count of stationary cars
+        self.colision = False     # Flag indicating if the car has collided
+        self.tiempo_inicio = time.time()  # Start time of car simulation
+        self.tiempo_terminar = 0  # Elapsed time for the car's simulation
+        self.historic_velocidad = []  # List to store historical velocities
+        self.vel_max = 2.2        # Maximum velocity of the car
+        
+        # Start a thread for accelerating the car
         threading = th.Thread(target=self.acelerar, args=())
         threading.start()
 
-
+    # Method to simulate a collision
     def chocaste(self):
-        time.sleep(0.8)
+        time.sleep(0.8)  # Sleep to simulate the collision delay
         self.colision = True
+    
+    # Method to simulate the car moving forward
     def avanzar(self):
         if self.velocidad < 0:
-            self.velocidad = 0
-        if self.colision == True:
-            if self.next_car != None:
-                print("_"*10)
+            self.velocidad = 0  # Ensure velocity is non-negative
+        if self.colision:
+            if self.next_car is not None:
+                print("_" * 10)
                 print(f'Auto {self.nombre} choco con el auto {self.next_car.nombre}', self.x)
             else:
-                print("_"*10)
+                print("_" * 10)
                 print(f'Auto {self.nombre} choco en cadena', self.x)
             return
-        elif self.quieto == True:
-            # aca deberia frenar ahasta llegar a 0 
+        elif self.quieto:
             return
         else:
-            # si chocaste y frenaste, volves a arrancar
-
-            
             try:
-                #si lo estas chocando frena
-                if self.x >= self.next_car.x -1.5:
-                    self.velocidad = 0
+                if self.x >= self.next_car.x - 1.5:
+                    self.velocidad = 0  # Car has stopped due to collision
                     choque_yo = th.Thread(target=self.chocaste, args=())
                     choque_yo.start()
                     choque_next = th.Thread(target=self.next_car.chocaste, args=())
@@ -73,127 +72,88 @@ class Auto:
                     self.quieto = True
                     self.quieto_count += 1
                     self.next_car.quieto = True
-                    
-
-                    
-                    # self.x = self.next_car.x
-                #si estas a menos de 10 de distancia desacelera
-                # elif self.x >= self.next_car.x - 15:
-                #     self.x += self.velocidad /10 
-                # elif self.x >= self.next_car.x - 30:
-                #     self.x += self.velocidad /4  
-
-                #caso normla acelera en base a la velocidad aleotoria
                 else:
-                    self.x += self.velocidad
+                    self.x += self.velocidad  # Move the car forward
             except:
-                self.x += self.velocidad
-            # if self.x > self.x_max:
-            #     self.x = self.x_max
-            
+                self.x += self.velocidad  # Move the car forward
             self.historic_velocidad.append(self.velocidad)
-            if self.next_car != None and self.x > self.next_car.x:
-                self.x = self.next_car.x
+            if self.next_car is not None and self.x > self.next_car.x:
+                self.x = self.next_car.x  # Adjust position if the car overshoots
         self.tiempo_terminar = time.time() - self.tiempo_inicio
+
     def __str__(self):
-        if self.next_car == None:
+        if self.next_car is None:
             return f"Car {self.nombre}, color {self.color}: (x={self.x}, velocidad={self.velocidad}, Auto siguiente: None con vel: '')"
         return f"Car {self.nombre}, color {self.color}: (x={self.x}, velocidad={self.velocidad}, Auto siguiente: {self.next_car.nombre} con vel: {self.next_car.velocidad})"
-    
 
-
-
+    # Method to simulate acceleration and deceleration of the car
     def acelerar(self):
-
-        while self.x < self.x_max and self.colision == False: # and self.velocidad <= self.vel_max:
-            
-            # se desconcentra 
-            
-            if self.next_car == None:
-                if self.velocidad - self.mean < self.mean/10:
-                    acelerar = random.lognormvariate(1, 1)*(2*(self.mean - self.velocidad)/self.mean)
+        while self.x < self.x_max and not self.colision:
+            if self.next_car is None:
+                if self.velocidad - self.mean < self.mean / 10:
+                    acelerar = random.lognormvariate(1, 1) * (2 * (self.mean - self.velocidad) / self.mean)
                     if acelerar > self.aceleracion_max:
                         acelerar = self.aceleracion_max
-                    self.velocidad = self.velocidad + acelerar
+                    self.velocidad = self.velocidad + acelerar*0.1
                 else:
                     acelerar = random.normalvariate(0, 0.25)
                     if acelerar > self.aceleracion_max:
                         acelerar = self.aceleracion_max
-                    self.velocidad = self.velocidad + acelerar
-                    
-                
-            elif self.velocidad - self.mean < self.mean/10:
-
-                vel_n = self.next_car.velocidad -0.5
+                    self.velocidad = self.velocidad + acelerar*0.1
+            elif self.velocidad - self.mean < self.mean / 10:
+                vel_n = self.next_car.velocidad - 0.5
                 dist = self.next_car.x - self.x
                 if self.velocidad == vel_n:
                     tiempo = 100
                 else:
-                    tiempo = (dist/(self.velocidad - vel_n))
-                if tiempo > 0 and tiempo < 30 :
-                    frenado = 10/(3**(tiempo)+0.000001)  * (-1 * random.lognormvariate(5,1))  
-                    # print(f'freando del auto {self.nombre}: {frenado}')
+                    tiempo = (dist / (self.velocidad - vel_n))
+                if 0 < tiempo < 30:
+                    frenado = 10 / (3 ** (tiempo) + 0.000001) * (-1 * random.lognormvariate(5, 1))
                     if frenado < -self.frenado_max:
-                        # print('esto')
                         frenado = -self.frenado_max
                     self.velocidad = self.velocidad + frenado* 0.1
                     if self.velocidad < 0:
                         self.velocidad = 0
                 else:
                     vel = self.velocidad
-                   
-                    acelerar = random.lognormvariate(1, 1)*(2*(self.mean - self.velocidad)/self.mean)
+                    acelerar = random.lognormvariate(1, 1) * (2 * (self.mean - self.velocidad) / self.mean)
                     if acelerar > self.aceleracion_max:
                         acelerar = self.aceleracion_max
-                    self.velocidad = self.velocidad + acelerar*0.1
-                    # if vel < 0.5:
-                        # print('acelerando', self.velocidad, self.mean,self.nombre,self.x,vel,acelerar)
-
-                                  #en este caso sigue acelerando si puede
-                #tiempo es muy chico quiero que frene rapdifo
-                #si el timepo es mucho 
+                    self.velocidad = self.velocidad + frenado* 0.1
             else:
-                
-                vel_n = self.next_car.velocidad -0.5
+                vel_n = self.next_car.velocidad - 0.5
                 dist = self.next_car.x - self.x
                 if self.velocidad == vel_n:
                     tiempo = 100
                 else:
-                    tiempo = (dist/(self.velocidad - vel_n))
-                if tiempo > 0 and tiempo < 30:
-                    frenado = 10/(3**(tiempo)+0.000001 ) * (-1 * random.lognormvariate(5,1))  
-                    # print(f'freando del auto {self.nombre}: {frenado}')
+                    tiempo = (dist / (self.velocidad - vel_n))
+                if 0 < tiempo < 30:
+                    frenado = 10 / (3 ** (tiempo) + 0.000001) * (-1 * random.lognormvariate(5, 1))
                     if frenado < -self.frenado_max:
-                        # print('esto')
                         frenado = -self.frenado_max
                     self.velocidad = self.velocidad + frenado* 0.1
                     if self.velocidad < 0:
                         self.velocidad = 0
                 else:
-                    # print('acelerando')
-                    acelerar = random.normalvariate(0, 0.5) 
-                    if self.velocidad > 2.8:
-                        acelerar = - abs(acelerar)
+                    acelerar = random.normalvariate(0, 0.5)
+                    if self.velocidad > 100:
+                        acelerar = -abs(acelerar)
                     if acelerar > self.aceleracion_max:
                         acelerar = self.aceleracion_max
-                    self.velocidad = self.velocidad + acelerar*0.1
+                    self.velocidad = self.velocidad + frenado* 0.1
                     if self.velocidad < 0:
                         self.velocidad = 0
 
-            chance = random.uniform(0,10)
+            chance = random.uniform(0, 10)
             if chance < 0.005:
-                time.sleep(0.2)
-                print('se desconcentro', self.nombre,self.x)
-
+                time.sleep(0.2)  # Simulate driver distraction with a 0.2s delay
+                print('se desconcentro', self.nombre, self.x)
             elif chance < 0.0052:
-                time.sleep(0.4)
-                print('se desconcentro', self.nombre,self.x)
-
+                time.sleep(0.4)  # Simulate driver distraction with a 0.4s delay
+                print('se desconcentro', self.nombre, self.x)
             elif chance < 0.0053:
-                print('se desconcentro', self.nombre,self.x)
-                time.sleep(0.5)
+                print('se desconcentro', self.nombre, self.x)
+                time.sleep(3)   # Simulate extended driver distraction with a 3s delay
             else:
                 time.sleep(random.uniform(self.reaction_time_mean - 0.01, self.reaction_time_mean + 0.01))
-
-        
-            self.tiempo_terminar = time.time() - self.tiempo_inicio
+        self.tiempo_terminar = time.time() - self.tiempo_inicio
